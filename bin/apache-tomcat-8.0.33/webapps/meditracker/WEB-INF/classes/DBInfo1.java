6d63d01c334a82386246a338fab6acd16527a56f
ADITYA KUMAR <aditya93kumar@hotmail.com>
2016-06-04 22:58:17 +0530

    Add files via upload

--- bin/apache-tomcat-8.0.33/webapps/meditracker/WEB-INF/classes/DBInfo1.java
+++ bin/apache-tomcat-8.0.33/webapps/meditracker/WEB-INF/classes/DBInfo1.java
@@ -21,7 +21,7 @@ public class DBInfo1
 	
 	public static Connection getConnection()
 	{
-		String url="mysql://bef0b8b70be2d0:5a254e6c@us-cdbr-azure-central-a.cloudapp.net/meditracker";
+		String url="us-cdbr-azure-central-a.cloudapp.net/meditracker";
 		String username="bef0b8b70be2d0";
 		String password="5a254e6c";
 		try 
